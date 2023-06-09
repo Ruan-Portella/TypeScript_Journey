035b32b5c751d52afe1741cbef1021137366b799
Ruan-Portella <ruanmorales29@gmail.br>
2023-06-09 18:40:50 -0300

    Feat: Configura Webpack

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./src/webpack-ts/index.ts ***!
+  \*********************************/
+
+console.log('Hello World!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
