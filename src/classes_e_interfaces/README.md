f38d0d106a5bec75850d356de0d9b07f41d0627a
Ruan-Portella <ruanmorales29@gmail.br>
2023-06-09 20:55:26 -0300

    Feat: Adiciona Criação de Classe

--- src/classes_e_interfaces/README.md
+++ src/classes_e_interfaces/README.md
@@ -4,4 +4,8 @@
 
 ## Informações aprendidas
 
-- 1 `` -
+- 1 `class` - Cria classes e tipos;
+- 2 `public` - Deixa o atributo disponível e possível de ser alterada;
+- 3 `private` - Deixa o atributo indisponível e impossível de ser alterada;
+- 4 `readonly` - Deixa o atributo como sendo único, não podendo ser alterado;
+- 5 `protected` - Deixa o atributo apenas ser utilizado pela classe e pelas suas sub-classes;
---
+++ src/classes_e_interfaces/classes_ts/index.ts
@@ -0,0 +1,35 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  public adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  public mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Fulano', 'Silva');
+const colaborador2 = new Colaborador('Ciclano', 'Silva');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+console.log(empresa1);
+empresa1.mostrarColaboradores();
