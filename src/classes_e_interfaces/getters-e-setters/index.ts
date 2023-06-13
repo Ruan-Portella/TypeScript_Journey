4aeb134531d05b25fbe59fe8c71c9dfb1a6da0cb
Ruan-Portella <ruanmorales29@gmail.br>
2023-06-12 21:08:40 -0300

    Feat: Adiciona Getters e Setters

---
+++ src/classes_e_interfaces/getters-e-setters/index.ts
@@ -0,0 +1,20 @@
+export class Pessoa {
+  constructor(
+    private nome: string,
+    private sobrenome: string,
+    private idade: number,
+    private _cpf: string,
+  ) {}
+
+  set cpf(cpf: string) {
+    this._cpf = cpf;
+  }
+
+  get cpf(): string {
+    return this._cpf.replace(/\D/g, '');
+  }
+}
+
+const pessoa = new Pessoa('ruan', 'portella', 20, '000.000.000-99');
+pessoa.cpf = '123.456.789-00';
+console.log(pessoa.cpf);
