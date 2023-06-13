af6f72d58774ca7d770573a8dc9f3828e890bff2
Ruan-Portella <ruanmorales29@gmail.br>
2023-06-12 21:21:05 -0300

    Feat: Adiciona Exemplo de Static

--- src/classes_e_interfaces/static_ts/index.ts
+++ src/classes_e_interfaces/static_ts/index.ts
@@ -1,11 +1,20 @@
 export class Pessoa {
+  static idadePadrao = 0;
+  static cpfPadrao = '000.000.000-00';
+
   constructor(
     public nome: string,
     public sobrenome: string,
     public idade: number,
     public cpf: string,
   ) {}
+
+  static criaPessoa(nome: string, sobrenome: string): Pessoa {
+    return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
 }
 
 const pessoa = new Pessoa('ruan', 'portella', 20, '000.000.000-99');
+const pessoa2 = Pessoa.criaPessoa('Ruan', 'Portella');
 pessoa.cpf = '123.456.789-00';
+console.log(pessoa2);
